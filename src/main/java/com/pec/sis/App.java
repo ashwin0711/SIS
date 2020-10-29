647d93756b33f641b3f65810b1a56dc3c379e043
ashwin0711 <ashwinjoe711@gmail.com>
2020-10-29 17:29:45 +0530

    first commit

---
+++ src/main/java/com/pec/sis/App.java
@@ -0,0 +1,12 @@
+package com.pec.sis;
+
+public class App 
+{
+    @SuppressWarnings("static-access")
+	public static void main( String[] args )
+    {
+        StudentinformationSystem app = new StudentinformationSystem();
+        app.StudentInfo();
+    }
+    
+}
